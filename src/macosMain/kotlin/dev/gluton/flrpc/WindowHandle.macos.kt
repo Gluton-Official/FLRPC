1bcbcdb086f30bed9f721a8537847ae7fe7e6e69
Gluton <glutonofficial@gmail.com>
2023-07-20 16:02:28 -0400

    Remove message in TODO()

--- src/macosMain/kotlin/dev/gluton/flrpc/WindowHandle.macos.kt
+++ src/macosMain/kotlin/dev/gluton/flrpc/WindowHandle.macos.kt
@@ -1,3 +1,3 @@
 package dev.gluton.flrpc
 
-actual fun WindowHandle.Companion.fromExecutableName(executableName: String): WindowHandle? = TODO("Not yet implemented")
+actual fun WindowHandle.Companion.fromExecutableName(executableName: String): WindowHandle? = TODO()
